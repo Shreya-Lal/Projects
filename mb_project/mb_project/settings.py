b365b65558ae37be97e13192f0f50683bbf163ce
Shreya <shreya.lal28@gmail.com>
2020-06-19 12:02:39 +0530

    New test Update

--- mb_project/mb_project/settings.py
+++ mb_project/mb_project/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = 'p-0nrk-@jib380s2c(7b7!k@4(qtsr_tauta^ul!0b)fkn%+47'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
